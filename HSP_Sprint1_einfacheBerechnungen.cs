50fb938a3b0ce88b87e1b5368bfa931abebd56b4
mo2271 <61688446+mo2271@users.noreply.github.com>
2020-04-02 15:07:52 +0200

    Update HSP_Sprint1_einfacheBerechnungen.cs

--- HSP_Sprint1_einfacheBerechnungen.cs
+++ HSP_Sprint1_einfacheBerechnungen.cs
@@ -6,10 +6,17 @@ namespace HSP_Sprint1_einfacheBerechnungen
     {
         static void Main(string[] args)
         {
+            //Ich mach nur außen nicht ihnen
             //BITTE ICH MACH DICH SONST FERTIG
             //GitHub regt mich jetzt schon auf
             Änderung Mathis
             Console.WriteLine("asdf");
+<<<<<<< HEAD
+            
+            // mo2271
+=======
+            // Bin gespannt
+>>>>>>> 7b9ac5608ab6b3ad5ef9ede48296c7efefc158ce
         }
     }
 }
