8848a09be0e2a19a2528789dac67d42c0d66e2d2
ma5995 <ma5995>
2020-04-02 15:28:35 +0200

    Update HSP_Sprint1_einfacheBerechnungen.cs

--- HSP_Sprint1_einfacheBerechnungen.cs
+++ HSP_Sprint1_einfacheBerechnungen.cs
@@ -6,7 +6,36 @@ namespace HSP_Sprint1_einfacheBerechnungen
     {
         static void Main(string[] args)
         {
-            
+            double X, Y;            //Kantenlängen
+            double A;               //Flächeninhalt
+            double Xs, Ys;          //Flächenschwerpunktkoordinaten
+            double Ixx, Iyy;        //Flächenträgheitsmomente
+            string Basiseinheit;    //Abfrage der Einheit
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.WriteLine("Flächeninhalt:" + A + Basiseinheit+"^2");
+            Console.WriteLine("Flächenschwerpunktkoordinate in X Richtung:" + Xs +Basiseinheit);
+            Console.WriteLine("Flächenschwerpunktkoordinate in Y Richtung:" + Ys +Basiseinheit);
+            Console.WriteLine("Flächenträgheitsmoment um die x Achse:" + Ixx + Basiseinheit +"^4");
+            Console.WriteLine("Flächenträgheitsmoment um die y Achse:" + Iyy + Basiseinheit +"^4");
+            Console.ReadKey();
+
+
+
+
+
         }
     }
 }
