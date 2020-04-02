95595d449f379cd75515bb08499f1770017d0a9a
ma5995 <ma5995>
2020-04-02 15:04:49 +0200

    Update HSP_Sprint1_einfacheBerechnungen.cs

--- HSP_Sprint1_einfacheBerechnungen.cs
+++ HSP_Sprint1_einfacheBerechnungen.cs
@@ -6,12 +6,7 @@ namespace HSP_Sprint1_einfacheBerechnungen
     {
         static void Main(string[] args)
         {
-            //Ich mach nur außen nicht ihnen
-            //BITTE ICH MACH DICH SONST FERTIG
-            //GitHub regt mich jetzt schon auf
-            Änderung Mathis
-            Console.WriteLine("asdf");
-            // Bin gespannt
+           
         }
     }
 }
