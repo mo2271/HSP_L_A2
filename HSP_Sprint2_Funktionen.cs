2857327cf2293246df54db6a6f21b8d77da277d0
mo2271 <61688446+mo2271@users.noreply.github.com>
2020-04-09 10:02:11 +0200

    Add files via upload

---
+++ HSP_Sprint2_Funktionen.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HSP_Sprint2_Funktionen
+{
+    class HSP_Sprint2_Funktionen
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
