bc89de0476952c959de654a8d486e4ab1bd3d7ab
ma5995 <ma5995>
2020-04-09 10:15:00 +0200

    Update HSP_Sprint2_Funktionen.cs

--- HSP_Sprint2_Funktionen.cs
+++ HSP_Sprint2_Funktionen.cs
@@ -6,17 +6,45 @@ namespace HSP_Sprint2_Funktionen
     {
         static void Main(string[] args)
         {
-            double X, Y;            //Kantenlängen
-            double A;               //Flächeninhalt
+            double X, Y;            // Kantenlängen
+            double A;               // Flächeninhalt
             double R;               // Radius
             double W;               // Wandstärke des Profils
-            double Xs, Ys;          //Flächenschwerpunktkoordinaten
-            double Ixx, Iyy;        //Flächenträgheitsmomente
-            string Basiseinheit;    //Abfrage der Einheit
-          
+            double Xs, Ys;          // Flächenschwerpunktkoordinaten
+            double Ixx, Iyy;        // Flächenträgheitsmomente
+            string Basiseinheit;    // Abfrage der Einheit
+            string profilabfrage;   // Abfrage des Profiltyps
 
 
-           
+            Console.WriteLine("Wählen Sie Ihren Profiltyp aus: [1]: Rechteck-Vollprofil, [2]: Rechteck-Hohlprofil, [3]: Kreis-Vollprofil, [4]: Kreis-Hohlprofil");
+
+            profilabfrage = Convert.ToString(Console.ReadLine());
+
+
+            if (profilabfrage == "1")       //Rechteck Vollprofil
+            {
+
+            }
+
+            else if (profilabfrage=="2")    //Rechteck Hohlprofil
+            {
+
+            }
+
+            else if (profilabfrage=="3")    //Kreis-Vollprofil
+            {
+
+            }
+
+            else if (profilabfrage=="4")    //Kreis-Hohlprofil
+            {
+
+            }
+
+            else
+            {
+                Console.WriteLine("Sie haben keine korrekte Ziffer für einen Profiltypen eingegeben!");
+            }
              
       
 
