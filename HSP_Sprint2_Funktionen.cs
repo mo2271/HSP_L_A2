2abee1b15c9fa875b78c8e4d41ce0896e643690f
ma5995 <ma5995>
2020-04-09 10:08:00 +0200

    Update HSP_Sprint2_Funktionen.cs

--- HSP_Sprint2_Funktionen.cs
+++ HSP_Sprint2_Funktionen.cs
@@ -6,7 +6,24 @@ namespace HSP_Sprint2_Funktionen
     {
         static void Main(string[] args)
         {
-            
+            double X, Y;            //Kantenlängen
+            double A;               //Flächeninhalt
+            double R;               // Radius
+            double W;               // Wandstärke des Profils
+            double Xs, Ys;          //Flächenschwerpunktkoordinaten
+            double Ixx, Iyy;        //Flächenträgheitsmomente
+            string Basiseinheit;    //Abfrage der Einheit
+          
+
+
+           
+             
+      
+
+
         }
     }
 }
+
+
+
