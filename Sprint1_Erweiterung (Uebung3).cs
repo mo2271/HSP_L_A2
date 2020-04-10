00b2f636b267c48b3860048f47c0eb96dbaaf341
mathis623 <mathis.tietjen38@gmail.com>
2020-04-10 14:38:00 +0200

    Update Sprint1_Erweiterung (Uebung3).cs

--- Sprint1_Erweiterung (Uebung3).cs
+++ Sprint1_Erweiterung (Uebung3).cs
@@ -30,7 +30,27 @@ namespace Sprint1_Erweiterung__Uebung3_
 
             if (profilabfrage == "1")       //Rechteck Vollprofil
             {
+                Console.WriteLine("Bitte geben Sie die Kantenlänge X ein:");    //Eingabe für X,Y und Basiseinheit.
+                X = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Bitte geben Sie die Kantenlänge Y ein:");
+                Y = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Bitte geben Sie eine Basiseinheit ein:");
+                Basiseinheit = Convert.ToString(Console.ReadLine());
+                Console.WriteLine("Start des Programms");
 
+                A = X * Y;      // Fläche Rechteck
+
+                Xs = X / 2;     // SWP-Koordinate von der unteren linken Ecke
+                Ys = Y / 2;     // SWP-Koordinate von der unteren linken Ecke
+
+                Ixx = (X * (Y * Y * Y)) / 12;   // Berechnung FTM um die X-Achse
+                Iyy = (Y * (X * X * X)) / 12;   // Berechnung FTM um die Y-Achse
+
+                Console.WriteLine("Flächeninhalt:" + A + Basiseinheit + "^2");
+                Console.WriteLine("Flächenschwerpunktkoordinate in X Richtung (KO-System Ursprung unten links):" + Xs + Basiseinheit);
+                Console.WriteLine("Flächenschwerpunktkoordinate in Y Richtung (KO-System Ursprung unten links):" + Ys + Basiseinheit);
+                Console.WriteLine("Flächenträgheitsmoment um die x Achse:" + Ixx + Basiseinheit + "^4");
+                Console.WriteLine("Flächenträgheitsmoment um die y Achse:" + Iyy + Basiseinheit + "^4");
             }
 
             else if (profilabfrage == "2")    //Rechteck Hohlprofil
