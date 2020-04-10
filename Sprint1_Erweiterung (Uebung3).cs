6090e67c79bc4642f2c486e4bdd57221907e4e77
ma5995 <ma5995>
2020-04-10 11:48:03 +0200

    Update Sprint1_Erweiterung (Uebung3).cs

--- Sprint1_Erweiterung (Uebung3).cs
+++ Sprint1_Erweiterung (Uebung3).cs
@@ -6,10 +6,59 @@ using System.Threading.Tasks;
 
 namespace Sprint1_Erweiterung__Uebung3_
 {
-    class Program
+    class Sprint1_Erweiterung__Uebung3_
     {
         static void Main(string[] args)
         {
+            double X, Y;            // Kantenlängen
+            double A;               // Flächeninhalt
+            double X, Y;            // Kantenlängen
+            double A;               // Flächeninhalt
+            double R;               // Radius
+            double W;               // Wandstärke des Profils
+            double Xs, Ys;          // Flächenschwerpunktkoordinaten
+            double Ixx, Iyy;        // Flächenträgheitsmomente
+            string Basiseinheit;    // Abfrage der Einheit
+            string profilabfrage;   // Abfrage des Profiltyps
+
+
+
+            Console.WriteLine("Wählen Sie Ihren Profiltyp aus: [1]: Rechteck-Vollprofil, [2]: Rechteck-Hohlprofil, [3]: Kreis-Vollprofil, [4]: Kreis-Hohlprofil");
+
+            profilabfrage = Convert.ToString(Console.ReadLine());
+
+
+            if (profilabfrage == "1")       //Rechteck Vollprofil
+            {
+
+            }
+
+            else if (profilabfrage == "2")    //Rechteck Hohlprofil
+            {
+
+            }
+
+            else if (profilabfrage == "3")    //Kreis-Vollprofil
+            {
+
+            }
+
+            else if (profilabfrage == "4")    //Kreis-Hohlprofil
+            {
+
+            }
+
+            else
+            {
+                Console.WriteLine("Sie haben keine korrekte Ziffer für einen Profiltypen eingegeben!");
+            }
+
+
+
         }
+
     }
+
+
 }
+
